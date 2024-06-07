b6e2b2d03c323f309fe9a3f2d91a668a13b22b27
putuandi <fergusoboy123@gmail.com>
2024-06-07 21:50:31 +0800

    melakukan update

--- app.py
+++ app.py
@@ -1,5 +1,21 @@
-from flask import Flask, redirect, url_for, render_template, request
+from flask import Flask, redirect, url_for, render_template, request, jsonify
 from pymongo import MongoClient
+import requests
+from datetime import datetime, timedelta
+import hashlib
+
+import os
+from os.path import join, dirname
+from dotenv import load_dotenv
+
+dotenv_path = join(dirname(__file__), '.env')
+load_dotenv(dotenv_path)
+
+MONGODB_URI = os.environ.get("MONGODB_URI")
+DB_NAME =  os.environ.get("DB_NAME")
+
+client = MongoClient(MONGODB_URI)
+db = client[DB_NAME]
 
 app = Flask(__name__)
 
