26892e56d31b485c16cc90ad2e269be79c567c8e
rizkyhasyim <2112500877@student.budiluhur.ac.id>
2024-06-07 10:01:28 +0700

    menambahkan sedikit perubahan

---
+++ app.py
@@ -0,0 +1,35 @@
+from flask import Flask, redirect, url_for, render_template, request
+from pymongo import MongoClient
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return render_template('home.html')
+
+@app.route('/about')
+def about():
+    return render_template('about.html')
+
+@app.route('/login')
+def login():
+    return render_template('login.html')
+
+@app.route('/register')
+def register():
+    return render_template('register.html')
+
+@app.route('/products')
+def index():
+    return render_template("products.html")
+
+@app.route('/all-products')
+def all_products():
+    return render_template("all-products.html")
+
+@app.route('/detail-product')
+def detail_product():
+    return render_template("detail-product.html")
+
+if __name__ == '__main__':
+    app.run(port=5000, debug=True)
